8b18d300e6e687e69fb7ad95ea3a3ff1ff866ce4
Nick Phelps <Nick90052@Gmail.com>
2018-12-07 09:54:22 -0600

    Problem 4

---
+++ require_statements_4/index.js
@@ -0,0 +1,9 @@
+let myNumbers = require('./add.js')
+
+//Use that function to console log 1+1, 200+100, and 123 + 321
+
+console.log( myNumbers(1,1) )
+
+console.log( myNumbers(200,100) )
+
+console.log( myNumbers(123,321) )
